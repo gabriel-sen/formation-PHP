32568214ee28572b956935c64add4407bbb999f0
gabriel sen <salut.gaby@gmail.com>
2021-01-20 02:58:18 +0100

    les fonctions

--- bases/fonction.php
+++ bases/fonction.php
@@ -7,10 +7,30 @@
 
     // Ecrire une fonction
 
-function dirBonjour($nom){
-    echo 'Bonjour'.$nom;
-}
+    function dirBonjour($nom){
+        echo 'Bonjour'.$nom;
+    }
 
-dirBonjour('Marie');
+    dirBonjour('Marie');
+
+    // exemples de fonctions
+
+    strtoupper(); // passe le string en maj
+    strtolower(); // passe le string en min
+    ucwords(); // premiere lettre des mots en maj
+
+    // sprintf , "%" et "d" signifie qu'on veux afficher un décimal "0" signifique qu'on mettras un zero si les cases sont pas remplie, "2", "2", "4" sont le nombre de nombres à afficher. Après la "," on attribue les valeurs. Ex:
+    echo sprintf('%02d/%02d/%04d', 1, 2, 1989);
+
+    echo"<br>";
+
+    $x = 123.456;
+    echo number_format($x, 1) // permets de définir combien de nombre après la virgul on affiche arrondis au dessus.
+
+
+    echo '<br>';
+
+    $mail = 'gabriel@gmail.com';
+    $position = strpos($mail , '@'); // on cherche la position dans le mail du caractère arobase
 
 ?>
